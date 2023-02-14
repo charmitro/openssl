18bff595f783d77dba3b81d90f11720ef6796965
Christoph Müllner <christoph.muellner@vrull.eu>
2023-02-14 06:22:03 +0100

    riscv: Add basic vector extension support
    The RISC-V vector extension comes with an implementation-defined
    number of bits per vector register (VLEN), which can be read out at
    run-time using the CSR 'vlenb' (which returns VLEN/8) followed by a
    multiplication by 8 (to convert bytes to bits).
    
    This patch introduces a RISC-V capability 'V' to specify the
    availability of the vector extension. If this extension is found at
    run-time, then we read out VLEN as described above and cache it.
    Caching ensures that we only read the CSR once at startup.
    This is necessary because reading out CSR can be expensive
    (e.g. if CSR readout is implemented using trap-and-emulate).
    
    Follow-up patches can make use of VLEN and chose the best strategy
    based on the available length of the vector registers.
    
    Signed-off-by: Christoph Müllner <christoph.muellner@vrull.eu>

--- include/crypto/riscv_arch.h
+++ include/crypto/riscv_arch.h
@@ -61,4 +61,7 @@ static const size_t kRISCVNumCaps =
 #define RISCV_HAS_ZBKB_AND_ZKND_AND_ZKNE() (RISCV_HAS_ZBKB() && RISCV_HAS_ZKND() && RISCV_HAS_ZKNE())
 #define RISCV_HAS_ZKND_AND_ZKNE() (RISCV_HAS_ZKND() && RISCV_HAS_ZKNE())
 
+/* Get the size of a vector register in bits (VLEN). */
+size_t riscv_vlen(void);
+
 #endif
